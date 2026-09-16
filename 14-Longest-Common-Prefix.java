919a84d31109b8e61e140dc6738b21087a709ce2
Sansu-glitch <sanskarsingh4027@gmail.com>
2026-09-16 11:04:30 +0530

    [14] [Time Beats: 59.99%] [Memory Beats: 32.33%] - LeetPush

--- 14-Longest-Common-Prefix.java
+++ 14-Longest-Common-Prefix.java
@@ -2,15 +2,14 @@ class Solution {
     public String longestCommonPrefix(String[] s) {
         for(int i = 0; i<s[0].length();i++) {
             for(int j = 1; j<s.length;j++) {
-                if(i>=s[j].length()|| s[0].charAt(i) != s[j].charAt(i)) {
+                if(i>=s[j].length() || s[0].charAt(i)!= s[j].charAt(i)) {
+                    
                     return s[0].substring(0,i);
                 }
-
             }
 
         }
         return s[0];
-       
         
     }
 }
